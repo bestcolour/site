cc341fdb43703205c948febeb6440370b48dca02
bestcolour <guest3050@gmail.com>
2021-03-10 12:04:50 +0800

    Sideby side refactored.
    Now to change the js files using both sideby side & zigzag

--- src/Components/Common/SectionTypes/SideBySide/SideBySide.js
+++ src/Components/Common/SectionTypes/SideBySide/SideBySide.js
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./SideBySide.module.css";
 
-export default function SideBySide({ content, info, cssInfo })
+export default function SideBySide({ content, cssInfo })
 {
     //#region ---------- Template ----------
     // const content =
@@ -17,12 +17,7 @@ export default function SideBySide({ content, info, cssInfo })
     //     //If is flipped is set to true, the subject's default position (left side on desktop, doenst affect mobile) will be on the opposite side
     //     isFlipped: false
     // };
-    // const info  = 
-    // {
-    //     subjectBackgroundColor : "red"
-    //     ,
-    //     descriptionBackgroundColor:"blue"
-    // };
+
     // const cssInfo =
     // {
     //     cssStyle_flipped: cssModule.flipped
@@ -30,6 +25,10 @@ export default function SideBySide({ content, info, cssInfo })
     //     cssStyle_notFlipped: cssModule.notFlipped
     //      ,
     //      cssStyle_SideBySideElement : cssModule.SideBySideElement
+    // ,
+    // cssStyle_descriptionBg : cssModule.descriptionBgStyle
+    // ,
+    // cssStyle_subjectBg : cssModule.subjectBgStyle
     // }
 
     //#endregion
@@ -38,8 +37,8 @@ export default function SideBySide({ content, info, cssInfo })
 
     //#region ------------- Root Style -----------------------
     let rootStyle;
-    //If there is no cssInfo added, then use default
-    if (cssInfo === undefined)
+    //If there is no cssInfo added or no cssStyle of flipped/notflipped, then use default
+    if (cssInfo === undefined || cssInfo.cssStyle_flipped === undefined || cssInfo.cssStyle_notFlipped === undefined)
     {
         rootStyle = isFlipped ? style.notFlipped : style.flipped;
     }
@@ -50,29 +49,23 @@ export default function SideBySide({ content, info, cssInfo })
     //#endregion
 
     //#region -------------- Backgrounds Style ----------------------
-    const descriptionBgStyle =
-    {
-        backgroundColor: info === undefined ? "transparent" : info.descriptionBackgroundColor
-    }
+    const descriptionBgClass = cssInfo !== undefined && cssInfo.cssStyle_descriptionBg !== undefined ? cssInfo.cssStyle_descriptionBg : style.descriptionBgColor_Default;
 
-    const subjectBgStyle =
-    {
-        backgroundColor: info === undefined ? "transparent" : info.subjectBackgroundColor
-    }
+    const subjectBgClass = cssInfo !== undefined && cssInfo.cssStyle_subjectBg !== undefined ? cssInfo.cssStyle_subjectBg : style.subjectBgColor_Default;
     //#endregion
 
     //#region ----------------- SideBySide Element Style ------------------------
-    const sideBySideElementStyle = cssInfo !== undefined && cssInfo.cssStyle_SideBySideElement !== undefined? cssInfo.cssStyle_SideBySideElement : style.sideBySideElement;
+    const sideBySideElementStyle = cssInfo !== undefined && cssInfo.cssStyle_SideBySideElement !== undefined ? cssInfo.cssStyle_SideBySideElement : style.sideBySideElement;
     //#endregion
 
 
 
     return (
         <div className={`${rootStyle} ${style.sideBySideRoot}`}>
-            <span className={sideBySideElementStyle} style={subjectBgStyle}>
+            <span className={`${sideBySideElementStyle} ${subjectBgClass}`} >
                 {subject}
             </span>
-            <span className={sideBySideElementStyle} style={descriptionBgStyle}>
+            <span className={`${sideBySideElementStyle} ${descriptionBgClass}`} >
                 {description}
             </span>
         </div>
