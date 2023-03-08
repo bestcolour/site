b957e563d71bfbe3262fece99f8f405520f85df6
bestcolour <49665281+bestcolour@users.noreply.github.com>
2023-03-08 11:53:52 +0800

    Update ProjectPage_PageDescriptionContent.js

--- src/Data/PageData/ProjectPage/ProjectPage_PageDescriptionContent.js
+++ src/Data/PageData/ProjectPage/ProjectPage_PageDescriptionContent.js
@@ -1,7 +1,7 @@
 const PageDescriptionContent_ProjectPage =
 {
-    pageTitle: "Project Table (Work In Progress)"
+    pageTitle: "My Projects"
     ,
-    description: "In this page, I store all of my past projects. The good and the bad are all in here as I think it is good to learn from mistakes and keep doing what is right."
+    description: "In this page, I store all of my past projects."
 }
 export default PageDescriptionContent_ProjectPage;
